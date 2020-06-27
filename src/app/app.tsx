45290fc3028cf7fa703a4611de2753b2def806dd
ConnorLacy <lacyconnor1409@gmail.com>
2020-06-27 12:44:50 -0400

    Comments at top of module

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-
 /*
     This is the React application we will be rendering on the server
     and serving to the client as a string -- then binding the react/js
     to the dom elements
 */
+
+import React from 'react'
 
 export const App = () => {
     return (<h1>This is the app!</h1>)
